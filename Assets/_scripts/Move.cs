7efce27940b0d2d62c000d4dcb4b69a90e0c5d3c
Robert Mroch <Robert.Mroch@Epsilon.com>
2016-06-02 15:14:06 -0500

    add solution files

---
+++ Assets/_scripts/Move.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+    public float strength;
+
+    Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //Debug.Log("UpArrow");
+            Vector3 v3Force = strength * transform.forward;
+            rb.AddForce(v3Force);
+        }
+	}
+}
